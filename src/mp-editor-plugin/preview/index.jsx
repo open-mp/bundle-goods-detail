d0688098d7ba932a7fa3b72bd58684cb29d3a4dc
tsxuehu <tsxuehu@gmail.com>
2018-08-21 22:01:56 +0800

    init

---
+++ src/mp-editor-plugin/preview/index.jsx
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react';
+import './preview.pcss'
+
+
+export default class WhitespacePreview extends PureComponent {
+    render() {
+        const { instance } = this.props;
+        return (
+            <div className="bundle-goods-detail-preview-design-component-whitespace-preview">
+                商品动作
+            </div>
+        );
+    }
+}
+    
