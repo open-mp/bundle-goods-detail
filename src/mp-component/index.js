0624c146b465b5f7f19b03d82a5fb78bed33844d
tsxuehu <tsxuehu@gmail.com>
2018-08-27 00:08:38 +0800

    init

--- src/mp-component/index.js
+++ src/mp-component/index.js
@@ -1,37 +1,52 @@
 // components/goods/goods.js
 Component({
-  properties: {
-    id: String 
-  },
+    properties: {
+        id: String
+    },
 
-  data: {
-    pullDownCount: 0
-  },
-  pageUtil: null,
-  methods: {
-    start({ pageUtil }) {
-      this.pageUtil = pageUtil;
+    data: {
+        count: 0
     },
-    receiveData(data){
-      console.log(data)
+    pageUtil: null,
+    methods: {
+        start({pageUtil}) {
+            this.pageUtil = pageUtil;
+        },
+        receiveData(data) {
+            console.log(data)
+        },
+        onPullDownRefresh() {
+            this.setData({count: ++this.data.count});
+            wx.stopPullDownRefresh();
+        },
+        fireEvent() {
+            this.pageUtil.emit('pull-down-count', this.data.count);
+        },
+        getGoodsDetail() {
+            return {
+                count: this.data.count
+            }
+        },
+        decrease() {
+            this.setData({count: --this.data.count});
+        },
+        add() {
+            this.setData({count: ++this.data.count});
+        }
     },
-    onPullDownRefresh() {
-      this.setData({ pullDownCount: ++this.data.pullDownCount});
-      wx.stopPullDownRefresh();
+    created() {
     },
-    fireEvent() {
-      this.pageUtil.emit('pull-down-count', this.data.pullDownCount);
-    }
-  },
-  created () {},
-  attached () {
-    this.triggerEvent("inited", {
-      instance: this,
-      id: this.id,
-      interfaces: ['goods'], // 组件实现的接口 其他组件可以通过接口名查询实现组件的接口
-      pageEvent: ["pull-down-refresh"]
-    })
-  },
-  moved() { },
-  detached () { },
-})
+    attached() {
+        this.triggerEvent("inited", {
+            instance: this,
+            id: this.id,
+            services: ['goods-detail'], // 组件实现的接口 其他组件可以通过接口名查询实现组件的接口
+        })
+    },
+    moved() {
+    },
+
+    detached() {
+        this.pageUtil.removeComponent(this.id);
+    },
+});
